fca9dbe6ddb6ed1853808b284760221596ae5955
hardikj2003 <68813211+hardikj2003@users.noreply.github.com>
2023-07-06 21:40:15 +0530

    Update myContract.sol

--- myContract.sol
+++ myContract.sol
@@ -1,25 +1,17 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-contract MyContract {
-    // Define the three error handlers.
-    function require(bool condition) public {
-        if (!condition) {
-            revert("Condition not met");
+
+contract TripleErrorHandler {
+    uint256 public totalSupply;
+    
+    function mint(uint256 amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        
+        totalSupply += amount;
+        
+        assert(totalSupply > 0);
+        
+        if (totalSupply > 100) {
+            revert("Total supply exceeds the limit");
         }
-    }
-    function assert(bool condition) public {
-        require(condition, "Condition not met");
-    }
-    function revert(string memory message) public {
-        throw new Error(message);
-    }
-    // Define the main function of the contract.
-    function main() public {
-        // Use require() to check if a condition is met.
-        require(msg.sender == owner(), "Only the owner can call this function");
-        // Use assert() to check if a condition is met and revert if it is not.
-        assert(balanceOf(msg.sender) >= 1000, "Not enough funds");
-        // Use revert() to revert the transaction with a custom message.
-        revert("Transaction reverted");
     }
 }
