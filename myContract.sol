7b0d47be0dfe061757456e0e4476faf8bb7005c8
hardikj2003 <68813211+hardikj2003@users.noreply.github.com>
2023-07-06 21:35:55 +0530

    Update myContract.sol

--- myContract.sol
+++ myContract.sol
@@ -1,26 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-contract ErrorHandling {
-   
-    function Assertfun(uint num) public pure{
-        assert(num!=0);
-    }
-
-    function divide(uint _numerator, uint _denomenator) public pure returns (uint){
-        if(_numerator<_denomenator){
-           
-            revert("Pls input numerator greater than denomenator.");
-            
+pragma solidity ^0.8.0;
+contract MyContract {
+    // Define the three error handlers.
+    function require(bool condition) public {
+        if (!condition) {
+            revert("Condition not met");
         }
-        return _numerator/_denomenator;
-       
-
     }
-    function multiple(uint a) public view returns (uint){
-        require(a>0,"Enter more then 0.");
-        return a*mult;
-
+    function assert(bool condition) public {
+        require(condition, "Condition not met");
     }
-uint mult=3;
+    function revert(string memory message) public {
+        throw new Error(message);
+    }
+    // Define the main function of the contract.
+    function main() public {
+        // Use require() to check if a condition is met.
+        require(msg.sender == owner(), "Only the owner can call this function");
+        // Use assert() to check if a condition is met and revert if it is not.
+        assert(balanceOf(msg.sender) >= 1000, "Not enough funds");
+        // Use revert() to revert the transaction with a custom message.
+        revert("Transaction reverted");
+    }
 }
