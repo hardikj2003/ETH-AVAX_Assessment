7e49c3226dba7288d484ca0182c4204b6a3fe5c9
hardikj2003 <68813211+hardikj2003@users.noreply.github.com>
2023-07-07 17:30:54 +0530

    Update myContract.sol

--- myContract.sol
+++ myContract.sol
@@ -1,17 +1,12 @@
 pragma solidity ^0.8.0;
 
-contract TripleErrorHandler {
-    uint256 public totalSupply;
-    
-    function mint(uint256 amount) public {
-        require(amount > 0, "Amount must be greater than zero");
-        
-        totalSupply += amount;
-        
-        assert(totalSupply > 0);
-        
-        if (totalSupply > 100) {
-            revert("Total supply exceeds the limit");
+contract ErrorContract {
+    function handleError(bool condition) public pure {
+        if (condition) {
+            require(condition, "Require error: Condition not met");
+            assert(condition);
+        } else {
+            revert("Revert error: Condition not met");
         }
     }
 }
