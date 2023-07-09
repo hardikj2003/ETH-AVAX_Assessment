6fb1d8fa519b62450ad79893da1a916a5735c405
hardikj2003 <68813211+hardikj2003@users.noreply.github.com>
2023-07-09 16:22:19 +0530

    Update myContract.sol

--- myContract.sol
+++ myContract.sol
@@ -1,18 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
-contract TripleErrorHandler {
-    uint256 public totalSupply;
-    
-    function mint(uint256 amount) public {
-        require(amount > 0, "Amount must be greater than zero");
-        
-        totalSupply += amount;
-        
-        assert(totalSupply > 0);
-        
-        if (totalSupply > 100) {
-            revert("Total supply exceeds the limit");
+contract ErrorHandlingContract {
+
+    uint gravity = 10;
+
+    function handler(uint _mass, uint _gravity) public view returns (uint) {
+        require(_mass > 0,"Mass of the object Cannot be 0.");
+
+        assert(_gravity == gravity);
+
+        if ((_mass * _gravity) < 0){
+            revert("Weight of the object cannot be zero");
         }
+
+        return _mass * _gravity;
     }
 }
